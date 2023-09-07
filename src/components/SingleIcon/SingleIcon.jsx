9f0af8fb08db784996acb48fbbbdab0e8a3aaf4d
babriwala <hanzalafarhann@gmail.com>
2023-09-07 10:52:51 +0600

    Single Icon Styling Done

--- src/components/SingleIcon/SingleIcon.jsx
+++ src/components/SingleIcon/SingleIcon.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
-
+import './SingleIcon.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SingleIcon = ({ icon }) => {
+  const status = icon.status;
   return (
-    <div>
-      <button style={{ display: "inline-block", fontSize: "40px" }}>
+    <div className="icon__item">
+      <button className="icon__btn">
         <FontAwesomeIcon icon={icon[1]} />
       </button>
-      <p>Font Name: {icon[1].iconName}</p>
-      <p>{icon.status}</p>
+      <p className="icon__name">Font - {(icon[1]?.iconName)?.toUpperCase()}</p>
+      <p className={status == "free" ? "free" : status == 'paid' ? "paid" : "others"}>{icon.status}</p>
+      <button className="icon__show-details">Details</button>
     </div>
   );
 };
